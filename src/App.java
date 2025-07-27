c906fe5743648d35ee2de78863b8b672357a35ad
new.Lich <lichston1@gmail.com>
2025-07-27 15:10:52 -0300

    Changing place

--- src/App.java
+++ src/App.java
@@ -1,4 +1,13 @@
 public class App {
+
+    public App(){
+        
+        
+
+    }
+
+
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
