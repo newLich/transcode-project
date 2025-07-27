0c22d1b14ddee8b57eeb45918ef73facf020e619
new.Lich <lichston1@gmail.com>
2025-07-27 20:04:51 -0300

    Alteração no nome dos arquvis do projeto

--- Transcoder/src/main/java/com/loder/Transcoder/TranscoderApplication.java
+++ Transcoder/src/main/java/com/loder/Transcoder/TranscoderApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.loder.Transcoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TranscoderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TranscoderApplication.class, args);
 	}
 
 }
